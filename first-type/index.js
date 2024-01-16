a2789b5a3adffc99764eee683a2ed083857ef263
SR-Rony <srrony707@gmail.com>
2024-01-16 23:34:05 +0600

    typescript setup and first program run

---
+++ first-type/index.js
@@ -0,0 +1,8 @@
+var firstName;
+firstName = "SR Rony";
+// console.log(firstName);
+// function
+function myFun(num1, num2) {
+    console.log(num1 + num2);
+}
+myFun(22, 22);
---
+++ first-type/index.ts
@@ -0,0 +1,12 @@
+let firstName:string
+firstName = "SR Rony"
+// console.log(firstName);
+
+// function
+
+function myFun (num1:number,num2:number) {
+    console.log(num1+num2);
+    
+}
+
+myFun(22,22)
