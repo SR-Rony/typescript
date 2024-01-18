2d1776b391a452917acfc31e94e5e4c6808a0f83
SR-Rony <srrony707@gmail.com>
2024-01-18 20:54:45 +0600

    user common type add

---
+++ user-type/type.js
@@ -0,0 +1,27 @@
+// build in type :Number , String , Boolean , null , undefind , Void
+var firstName;
+var lastName;
+var fullName;
+var userId;
+var acount;
+firstName = "SR";
+lastName = " Rony";
+fullName = firstName.concat(lastName);
+acount = true;
+userId = 101;
+// console.log(`name : ${fullName} id : ${userId} your acount : ${acount}`);
+var mainName;
+function display() {
+    // console.log("welcome to my typescript");
+}
+display();
+// array
+var users;
+users = ["sr", "rony", "anis", "akib", "imran", "sajol"];
+users.sort();
+// console.log(users);
+// tuple type
+var array;
+array = [1232, "amei"];
+array.push(243, "Sr");
+console.log(array);
---
+++ user-type/type.ts
@@ -0,0 +1,50 @@
+// build in type :Number , String , Boolean , null , undefind , Void
+
+let firstName : string;
+let lastName : string;
+let fullName : string
+let userId : number;
+let acount: boolean
+
+
+firstName = "SR"
+lastName = " Rony"
+fullName =firstName.concat(lastName)
+acount = true
+userId = 101
+
+
+// console.log(`name : ${fullName} id : ${userId} your acount : ${acount}`);
+
+let mainName : number | string
+
+
+function display () :void {
+    // console.log("welcome to my typescript");
+    
+}
+
+display()
+
+// array
+
+let users : string[]
+
+users =["sr", "rony", "anis","akib","imran","sajol"] 
+users.sort()
+// console.log(users);
+
+
+// tuple type
+
+let array :[number,string]
+
+array =[1232,"amei"]
+array.push(243,"Sr")
+console.log(array);
+
+
+
+
+
+
